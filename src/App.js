1821e43286534647750a33333d1420209e5745f5
mehul3446 <35975643+mehul3446@users.noreply.github.com>
2020-10-10 01:33:14 -0400

    ok

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import Home from "./Home";
 import React from "react";
 import "./App.css";
 import Header from "./Header";
-import { BrowserRouter as Route, Router, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Checkout from "./Checkout";
 
 function App() {
   return (
     <Router>
       <div className="app">
         <Switch>
+          <Route path="/Checkout">
+            <Header />
+            <Checkout />
+          </Route>
+
           <Route path="/">
             <Header />
             <Home />
